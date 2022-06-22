6a1c9b7e5af376e911368f8325383cb798240282
santhiya <santhiya30032@gmail.com>
2022-06-22 10:20:30 -0700

    react task 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let addtask = () => {
+    if(tasklist === " "){
+      alert("WELCOME" , window.location.reload())
+      
+    }
+    settodoList([
+      ...List,
+      { id: List.length + 1, name: `${tasklist}`, isDone: false },
+    ]);
+  };
+  let taskdone = (id) => {
+    let itemList = List.findIndex((obj) => obj.id === id);
+   
+    settodoList([...List]);
+    if (List[itemList].isDone === false) {
+      List[itemList].isDone = true;
+    } else if (List[itemList].isDone === true) {
+      List[itemList].isDone = false;
+    }
+  };
+  let deletetask = (id) => {
+    let removetask = List.findIndex((obj) => obj.id === id);
+    List.splice(removetask, 1);
+    settodoList([...List]);
+  };
+
+  const [tasklist, setTaskList] = useState("");
+  const [List, settodoList] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <h1>CREATE LIST</h1>
+      <input type="text" placeholder="Create your list😊" onChange={(e) => setTaskList(e.target.value)}></input>{" "}
+      <button onClick={addtask}> Create Task</button>
+
+      <ul>
+        {List.map((item) => {
+          return (
+            <li className= {item.isDone ? "markdone" : ""}>
+              <h5>{item.name}</h5>
+
+              <div>
+              <button onClick={() => taskdone (item.id)}> Done </button>
+              <button onClick={() => deletetask (item.id)}> Delete </button>
+              </div>
+              
+              
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default App ;
